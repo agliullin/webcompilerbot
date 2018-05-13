1903fb3fdcb74cca7b5f231029a02635815fba53
agliullin <albertagliullin@gmail.com>
2018-05-13 13:58:22 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '582781742:AAEdHk29QT-MAYqG8697qjocsY5hOUnzqgk'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello $(msg.from.firstname), it is compiler bot`)
+})
+
