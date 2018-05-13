5bcb374d09ecd9bd486fdeaf62e8f311fc6c0391
agliullin <albertagliullin@gmail.com>
2018-05-13 15:27:48 +0300

    Add new messages

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ const bot = new TelegramBot(TOKEN, {polling: true})
 bot.on('message', msg => {
     GetText = msg.text;
     ParseText = doParse(GetText);
+    bot.sendMessage(msg.chat.id, 'Parsing result: ');
+    bot.sendMessage(msg.chat.id, ParseText);
     IRText = doIR(ParseText);
+    bot.sendMessage(msg.chat.id, 'IR result: ');
+    bot.sendMessage(msg.chat.id, IRText);
     OptimizeText = doOptimize(IRText);
+    bot.sendMessage(msg.chat.id, 'Optimize result: ');
+    bot.sendMessage(msg.chat.id, OptimizeText);
     CompileText = doCompile(OptimizeText);
+    bot.sendMessage(msg.chat.id, 'Compile result: ');
+    bot.sendMessage(msg.chat.id, CompileText);
     ExecuteText = doExecute(CompileText);
+    bot.sendMessage(msg.chat.id, 'Execute result: ');
     bot.sendMessage(msg.chat.id, ExecuteText);
 })
 
