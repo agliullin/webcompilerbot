436c19b6191f0c9758a873d1d4de75de0593cfb7
agliullin <albertagliullin@gmail.com>
2018-05-13 15:47:05 +0300

    Add vars

--- index.js
+++ index.js
@@ -3590,20 +3590,20 @@ const TOKEN = '582781742:AAEdHk29QT-MAYqG8697qjocsY5hOUnzqgk'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    GetText = msg.text;
-    ParseText = doParse(GetText);
+    var GetText = msg.text;
+    var ParseText = doParse(GetText);
     bot.sendMessage(msg.chat.id, 'Parsing result: ');
     bot.sendMessage(msg.chat.id, ParseText);
-    IRText = doIR(ParseText);
+    var IRText = doIR(ParseText);
     bot.sendMessage(msg.chat.id, 'IR result: ');
     bot.sendMessage(msg.chat.id, IRText);
-    OptimizeText = doOptimize(IRText);
+    var OptimizeText = doOptimize(IRText);
     bot.sendMessage(msg.chat.id, 'Optimize result: ');
     bot.sendMessage(msg.chat.id, OptimizeText);
-    CompileText = doCompile(OptimizeText);
+    var CompileText = doCompile(OptimizeText);
     bot.sendMessage(msg.chat.id, 'Compile result: ');
     bot.sendMessage(msg.chat.id, CompileText);
-    ExecuteText = doExecute(CompileText);
+    var ExecuteText = doExecute(CompileText);
     bot.sendMessage(msg.chat.id, 'Execute result: ');
     bot.sendMessage(msg.chat.id, ExecuteText);
 })
