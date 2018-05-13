3f22dab5bca29b939b7cde50ef141763bb632c42
agliullin <albertagliullin@gmail.com>
2018-05-13 14:12:17 +0300

    Edit first_name in message

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const TOKEN = '582781742:AAEdHk29QT-MAYqG8697qjocsY5hOUnzqgk'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello ` + $(msg.from.firstname) + `, it is compiler bot`)
+    bot.sendMessage(msg.chat.id, `Hello $(msg.from.first_name), it is compiler bot`)
 })
 
