05a33d1ce0aa37bd69436fe093d41c920ced9c0c
agliullin <albertagliullin@gmail.com>
2018-05-13 17:15:39 +0300

    Edit index file

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-require('parse.js')
-require('ieee754.js')
-require('ir.js')
-require('pascal_demo.js')
-require('llvm.js/compiler.js')
 
-const TelegramBot = require('node-telegram-bot-api')
+require('parse.js');
+require('ieee754.js');
+require('ir.js');
+require('pascal_demo.js');
+require('llvm.js/compiler.js');
 
-const TOKEN = '582781742:AAEdHk29QT-MAYqG8697qjocsY5hOUnzqgk'
+const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(TOKEN, {polling: true})
+const TOKEN = '582781742:AAEdHk29QT-MAYqG8697qjocsY5hOUnzqgk';
+
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
     GetText = msg.text;
@@ -27,5 +28,5 @@ bot.on('message', msg => {
     ExecuteText = doExecute(CompileText);
     bot.sendMessage(msg.chat.id, 'Execute result: ');
     bot.sendMessage(msg.chat.id, ExecuteText);
-})
+});
 
