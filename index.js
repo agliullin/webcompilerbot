c7207c5850c0cf4ae64644d4cfebe1418aa3abca
agliullin <albertagliullin@gmail.com>
2018-05-14 17:01:55 +0300

    edit index file

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = '582781742:AAEdHk29QT-MAYqG8697qjocsY5hOUnzqgk';
 
 const bot = new TelegramBot(TOKEN, {polling: true});
 
+const Xray = require('x-ray');
+
+const request = require('request');
+
+const fs = require('fs');
+
+const xray = new Xray();
+
+var url = 'https://ru.wikipedia.org/wiki/';
+
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Privet');
+
+    xray(url + msg.text, '.infobox.vcard', [{
+        title: 'td',
+        content: 'td a'
+    }]).write('results.json');
 });
+
+
+
 
